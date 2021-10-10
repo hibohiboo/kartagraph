093cc9db913ea78b0cebf7a760c2a9a5ef9428f7
hibohiboo <swdice66@gmail.com>
2021-10-10 14:45:36 +0900

    add logo #134

--- client/src/components/pages/Top.tsx
+++ client/src/components/pages/Top.tsx
@@ -1,13 +1,52 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
+const css = {
+  body: {
+    height: '100vh',
+    width: '100vw',
+    margin: 0,
+    paddint: 0,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // font
+    backgroundColor: 'black',
+    color: '#eee',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif;",
+    // fontSmooth: 'antialiased', Mac のみなので使わない。
+    // -moz-osx-font-smoothing: grayscale;'
+  },
+  mainTitle: {
+    color: '#eee',
+    fontSize: '3rem',
+
+    boxSizing: 'border-box',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '5px',
+
+    textDecoration: 'none',
+  },
+  subtitle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+} as const
 
 const Top: React.FC = ({ children }) => (
-  <div>
+  <div style={css.body}>
     <div>
-      <span>hellow</span>
-      <Link to="/about">about</Link>
-      <Link to="/game">game</Link>
+      <span style={css.subtitle}>電脳ゲームブック</span>
+      <Link style={css.mainTitle} to="/game">
+        カルタグラフ
+      </Link>
+      <span style={css.subtitle}>君だけの旅路</span>
     </div>
   </div>
 )
