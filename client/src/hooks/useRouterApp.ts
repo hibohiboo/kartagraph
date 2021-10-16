f6104c155d2393e77089dde931a0ec22ed50c218
hibohiboo <swdice66@gmail.com>
2021-10-16 20:37:20 +0900

    fix package.json #140

---
+++ client/src/hooks/useRouterApp.ts
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+import { useAppSelector } from '@/store/hooks'
+import { isUserAuthenticatedSelector } from '@/store/selectors/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from '@/store/slices/auth'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '@/domain/firebase'
+
+const useRouterApp = () => {
+  const authenticated = useAppSelector(isUserAuthenticatedSelector)
+  const dispatch = useDispatch()
+
+  const refresh = React.useCallback(
+    async (uid) => {
+      const userData = {
+        uid,
+      }
+      return dispatch(login(userData))
+    },
+    [dispatch],
+  )
+  useEffect(() => {
+    const f = async () => {
+      onAuthStateChanged(auth, async (user) => {
+        if (!user) {
+          dispatch(login({}))
+        }
+        if (user && !authenticated) {
+          return await refresh(user.uid)
+        }
+        if (!user && !authenticated) {
+          dispatch(logout())
+        }
+      })
+      // await auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    }
+    f()
+  })
+  console.log(authenticated)
+  return { authenticated }
+}
+
+export default useRouterApp
--- client/src/router/RoutesApp.tsx
+++ client/src/router/RoutesApp.tsx
@@ -1,49 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
 import Top from '@/components/pages/Top'
 import Game from '@/components/pages/Game'
 import PrivacyPolicy from '@/components/pages/PrivacyPolicy'
 import Agreement from '@/components/pages/Agreement'
-import { useAppSelector } from '@/store/hooks'
-import { isUserAuthenticatedSelector } from '@/store/selectors/auth'
-import { useDispatch } from 'react-redux'
-import { login, logout } from '@/store/slices/auth'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '@/domain/firebase'
 import Loading from '@/components/atoms/Loading'
+import useRouterApp from '@/hooks/useRouterApp'
 
 function App() {
-  const authenticated = useAppSelector(isUserAuthenticatedSelector)
-  const dispatch = useDispatch()
+  const { authenticated } = useRouterApp()
 
-  const refresh = React.useCallback(
-    async (uid) => {
-      const userData = {
-        uid,
-      }
-      return dispatch(login(userData))
-    },
-    [dispatch],
-  )
-  useEffect(() => {
-    const f = async () => {
-      onAuthStateChanged(auth, async (user) => {
-        if (!user) {
-          dispatch(login({}))
-        }
-        if (user && !authenticated) {
-          return await refresh(user.uid)
-        }
-        if (!user && !authenticated) {
-          dispatch(logout())
-        }
-      })
-      // await auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
-    }
-    f()
-  })
-  console.log(authenticated)
   if (authenticated === undefined) {
     return <Loading />
   }
