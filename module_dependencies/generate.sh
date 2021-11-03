f81b81bd7dce01e85178ee43047f08e507408118
hibohiboo <hibohiboo@users.noreply.github.com>
2021-11-03 05:28:15 +0000

    deploy: 4f3195e496d4294e21bf121b88cf74987889e02b

--- module_dependencies/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# https://github.com/pahen/madge
-# https://github.com/tyankatsu0105/sandbox-redux-toolkit-create-entity-adapter
-# npx madge --image image.svg ../../client/src/router/RoutesApp.tsx RoutesAppしか表示されない
-# npx madge --image image.svg ../../client/src/index.tsx 2つだけ
-# npx madge --image image.svg ../../client/src/components/pages
