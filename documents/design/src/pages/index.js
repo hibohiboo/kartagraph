a56a7d1c61a8ec146941f36397a74bd64a433b70
hibohiboo <swdice66@gmail.com>
2021-11-03 15:09:24 +0900

    gatsyby 導入 #146

---
+++ documents/design/src/pages/index.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>設計書</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
