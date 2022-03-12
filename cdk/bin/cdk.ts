1f2e92fe6db46584f92e92b4928fe7f747c6710f
hibohiboo <swdice66@gmail.com>
2022-03-12 17:05:14 +0900

    fix cdk #152

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,24 +4,24 @@ import * as cdk from 'aws-cdk-lib'
 import { AWSCarTaGraphClientStack } from '../lib/cdk-stack'
 import * as dotenv from 'dotenv'
 
-// typescriptで予測が効かずにundefinedがとれない
-// ;(['PROJECT_ID', 'ROOT_DOMAIN', 'DEPLOY_DOMAIN'] as const).forEach((key) => {
-//   if (!process.env[key]) throw new Error(`please add ${key} to .env`)
-// })
 dotenv.config()
-if (!process.env.PROJECT_ID) throw new Error(`please add PROJECT_ID to .env`)
-if (!process.env.ROOT_DOMAIN) throw new Error(`please add ROOT_DOMAIN to .env`)
-if (!process.env.DEPLOY_DOMAIN)
-  throw new Error(`please add DEPLOY_DOMAIN to .env`)
-if (!process.env.TAG_PROJECT_NAME)
-  throw new Error(`please add TAG_PROJECT_NAME to .env`)
+const envList = [
+  'PROJECT_ID',
+  'ROOT_DOMAIN',
+  'DEPLOY_DOMAIN',
+  'TAG_PROJECT_NAME',
+] as const
+for (const key of envList) {
+  if (!process.env[key]) throw new Error(`please add ${key} to .env`)
+}
+const processEnv = process.env as Record<typeof envList[number], string>
 
 const app = new cdk.App()
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 }
-const projectId = process.env.PROJECT_ID
+const projectId = processEnv.PROJECT_ID
 
 new AWSCarTaGraphClientStack(app, `${projectId}-stack`, {
   bucketName: `${projectId}-s3-bucket`,
@@ -30,8 +30,8 @@ new AWSCarTaGraphClientStack(app, `${projectId}-stack`, {
   imageCachePolicyName: `${projectId}-cache-policy-image`,
   functionName: `${projectId}-lambda-edge-ogp`,
   distributionName: `${projectId}-distribution-cloudfront`,
-  rootDomain: process.env.ROOT_DOMAIN,
-  deployDomain: process.env.DEPLOY_DOMAIN,
-  projectNameTag: process.env.TAG_PROJECT_NAME,
+  rootDomain: processEnv.ROOT_DOMAIN,
+  deployDomain: processEnv.DEPLOY_DOMAIN,
+  projectNameTag: processEnv.TAG_PROJECT_NAME,
   env,
 })
