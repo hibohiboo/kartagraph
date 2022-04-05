6cdf9853f0faf59e39d7b41d18e90a5190470980
hibohiboo <swdice66@gmail.com>
2022-04-06 00:16:51 +0900

    fix update package.json #172

--- documents/components-catalog/.storybook/main.js
+++ documents/components-catalog/.storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
+  "typescript" : { reactDocgen: false }, // https://dev.classmethod.jp/articles/tried-to-add-storybook-to-nextjs-project/
   "stories": [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     '../stories/components/atoms/Loading.stories.tsx'
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
--- documents/components-catalog/.storybook/webpack.config.js
+++ documents/components-catalog/.storybook/webpack.config.js
@@ -3,14 +3,14 @@ const { resolve } = require('path')
 module.exports = ({ config }) => {
   config.resolve.extensions.push('.ts', '.tsx')
   config.resolve.alias['@'] = resolve(__dirname, '../../../client/src')
-  // storiesをtsxで書けるようにする
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('babel-loader'),
-    options: {
-      presets: [require.resolve('babel-preset-react-app')],
-    },
-  })
+  // // storiesをtsxで書けるようにする
+  // config.module.rules.push({
+  //   test: /\.(ts|tsx)$/,
+  //   loader: require.resolve('babel-loader'),
+  //   options: {
+  //     presets: [require.resolve('babel-preset-react-app')],
+  //   },
+  // })
   // 監視ポーリング設定(vagrant用)
   // config.watchOptions = {
   //   aggregateTimeout: 200,
